b1d7d487f400198e1d6ede95c7c3a675946aee20
Shreekz <alanmguadet@gmail.com>
2021-07-21 21:08:56 -0400

    added typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
-app.use(express.urlencoded({ extended: true }));
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
